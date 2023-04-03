612caca13f9185d06a4482b316cd59595f9249e6
Joy Karmakar <96533682+joy095@users.noreply.github.com>
2023-04-03 09:19:27 +0530

    cors

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const corsOptions = {
 };
 
 const app = express();
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 
 app.use("/api/v1/post", postRoutes);
