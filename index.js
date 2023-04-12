ff5e2be05f720c524738da711b1aa163c0cec4da
Joy Karmakar <96533682+joy095@users.noreply.github.com>
2023-04-12 08:09:36 +0530

    cors updated

--- index.js
+++ index.js
@@ -9,7 +9,13 @@ import dalleRoutes from "./routes/dalleRoutes.js";
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://slug-panel.onrender.com",
+    headers: ["Content-Type"],
+    credentials: true,
+  })
+);
 app.use(express.json({ limit: "50mb" }));
 
 app.use("/api/v1/post", postRoutes);
